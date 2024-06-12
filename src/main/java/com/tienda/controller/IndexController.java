1e51209d2a7d2b86da2837253556467b3fdb32f7
jacob-marin <marinjacob@gmail.com>
2024-06-11 21:02:52 -0600

    semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,21 @@
+
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author jacob
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+}
